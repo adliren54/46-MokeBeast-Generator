1eab50818775095d394fac4a1b81361d3218b7c4
adliren54 <62419296+adliren54@users.noreply.github.com>
2023-05-02 19:07:29 +0000

    Mokedex using 2D Dictionaries

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+print("🌟MokeBeast Generator🌟")
 
-john = {"daysCompleted": 46, "streak": 22}
-janet = {"daysCompleted": 21, "streak": 21}
-erica = {"daysCompleted": 75, "streak": 6}
+moke = {}
 
-courseProgress = {"John":john, "Janet":janet, "Erica":erica}
+def prettyPrint():
+  print()
+  for key, value in moke.items():
+    print(key, end=": ")
+    for subkey, subvalue in value.items():
+      print(subkey, subvalue, end=" | ")
+    print()
 
-print(courseProgress["Erica"]["daysCompleted"])
-print(courseProgress["Janet"]["streak"])
+while True:
+  name = input("\nInput your beast's name > ").strip().title()
+  type = input("\nInput your beast's type > ").strip().title()
+  move = input("\nInput your beast's special move > ").strip().title()
+  hp = input("\nInput your beast's staring HP > ").strip().title()
+  mp = input("\nInput your beast's staring MP > ").strip().title()
+  
+  moke[name] = {"Type":type, "Move":move, "HP":hp, "MP":mp}
+  
+  #if moke["Type"] == "Water":
+    #print("\033[44m", end="")
+  #elif moke["Type"] == "Fire":
+    #print("\033[31m", end="")
+  #elif moke["Type"] == "Air":
+    #print("\033[37m", end="")
+  #elif moke["Type"] == "Earth":
+    #print("\033[32m", end="")
+  #elif moke["Type"] == "Spirit":
+    #print("\033[45m", end="")
+
+  prettyPrint()
+  
+  proceed = input("Again? y/n > ").lower()
+  if proceed == "y":
+    continue
+  elif proceed == "n":
+    break
+  
+
+#figure out how to make the color of the letter
+
+#figure out to align the ":"
